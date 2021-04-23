44399740c9a275a4593843306b2a71361d6def9c
Drashi Jain <82202222+Drashidj@users.noreply.github.com>
2021-04-23 15:41:00 +0530

    created string_stream.cpp

---
+++ string_stream.cpp
@@ -0,0 +1,30 @@
+#include <sstream>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> parseInts(string str)
+{vector<int>arr;
+ stringstream ss(str);
+    
+ char ch;
+ int temp;
+ while(ss>>temp)
+ {
+    arr.push_back(temp);
+    ss>>ch;
+ }
+ return arr;
+}
+
+int main() {
+    string str;
+    cin >> str;
+    vector<int> integers = parseInts(str);
+    for(int i = 0; i < integers.size(); i++) {
+        cout << integers[i] << "\n";
+    }
+    
+    return 0;
+}
