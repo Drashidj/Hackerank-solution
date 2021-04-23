d9c3da9a0ba9ef7d24b45b8554fdb5af24766e88
Drashi Jain <82202222+Drashidj@users.noreply.github.com>
2021-04-23 15:42:24 +0530

    created strings.cpp

---
+++ strings.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	// Complete the program
+    string a,b;
+    cin>>a;
+    cin>>b;
+    cout<<a.size()<<" "<<b.size()<<endl;
+    string c=a+b;
+    cout<<c<<endl;
+    char ch =a[0];
+    a[0]=b[0];
+    b[0]= ch;
+    cout<<a<<" "<<b;
+  
+    return 0;
+}
